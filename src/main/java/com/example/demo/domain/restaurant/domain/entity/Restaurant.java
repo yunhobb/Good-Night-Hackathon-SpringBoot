e4ddca04868423597520dd06268b0f398a7d8c17
Jung Yunho <a01049048063@gmail.com>
2023-02-26 01:57:27 +0900

    feat: Restaurant Entity 생성

---
+++ src/main/java/com/example/demo/domain/restaurant/domain/entity/Restaurant.java
@@ -0,0 +1,40 @@
+package com.example.demo.domain.restaurant.domain.entity;
+
+import com.example.demo.domain.review.domain.entity.Review;
+import com.example.demo.global.domain.BasetimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Restaurant extends BasetimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "restaurant_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private Type type;
+
+    @OneToMany(mappedBy = "restaurant")
+    private List<Review> reviews = new ArrayList<>();
+
+    private Boolean isDeleted;
+
+    @Builder
+    public Restaurant(String name, Type type) {
+        this.name = name;
+        this.type = type;
+        this.isDeleted = false;
+    }
+}
